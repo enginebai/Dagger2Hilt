036c6e36a73ecfcc4355386bf30ef0725edf6d33
Engine Bai <enginebai@gmail.com>
2023-03-24 10:04:15 +0800

    Migrate all view models to hilt

--- app/src/main/java/com/enginebai/poc/MainViewModel.kt
+++ app/src/main/java/com/enginebai/poc/MainViewModel.kt
@@ -1,12 +1,12 @@
 package com.enginebai.poc
 
-import android.app.Application
 import android.content.Context
-import com.enginebai.core.base.BaseAndroidViewModel
 import com.enginebai.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
+@HiltViewModel
 class MainViewModel @Inject constructor(
     @ApplicationContext val context: Context
 ) : BaseViewModel() {
