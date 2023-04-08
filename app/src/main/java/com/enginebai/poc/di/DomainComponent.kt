ef489fcd87cc16200cbe9bcdd047bbf21b62fe01
Engine Bai <enginebai@gmail.com>
2023-04-08 11:19:22 +0800

    Provide Koin module from dynamic feature module

--- app/src/main/java/com/enginebai/poc/di/DomainComponent.kt
+++ app/src/main/java/com/enginebai/poc/di/DomainComponent.kt
@@ -6,6 +6,7 @@ import com.enginebai.core.util.ColorDefinition
 import com.enginebai.poc.MyApplication
 import com.enginebai.poc.data.DomainRepository
 import com.enginebai.poc.data.domain.DomainTopic
+import com.enginebai.poc.di.koin.KoinFacade
 import com.enginebai.poc.ui.widget.RandomTopicButton
 import com.enginebai.poc.ui.widget.RandomTopicItem
 import com.example.feature.data.CardRepository
@@ -41,6 +42,7 @@ interface DomainComponent {
      * 2. dagger provider itself or injector
      */
     fun domainRepository(): DomainRepository
+    fun koinFacade(): KoinFacade
 
     // Provide to dynamic feature
     fun domainTopic(): DomainTopic
